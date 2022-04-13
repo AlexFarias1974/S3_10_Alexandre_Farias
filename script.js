4bd4659eea6e852aeb20529fab0ab3aec897d64d
Alexandre Farias <alex_wfarias@yahoo.com.br>
2022-04-13 15:40:58 -0300

    Primeira alteração S3-10

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-let player1 = "Rafael"
-let player2 = "Manoel"
-let player3 = "Daniel"
-
 function positions(firstPlace, secondPlace, lastPlace) {
     let posicoes = [firstPlace, secondPlace, lastPlace]
 
     if (secondPlace === "Daniel") {
-        posicoes = [secondPlace, firstPlace, lastPlace]
+        posicoes[0] = posicoes[1]
+        posicoes[1] = firstPlace
     }
-            
+    
     else if (lastPlace === "Daniel") {
-        posicoes = [firstPlace, lastPlace, secondPlace]                
+        posicoes[1] = posicoes[2]
+        posicoes[2] = secondPlace
     }
-            
-    return "1º Colocado " + posicoes[0] + ", 2º Colocado " + posicoes[1] + ", 3º Colocado " + posicoes[2]
+    console.log(`1º - Colocado  ${posicoes[0]}`)
+    console.log(`2º - Colocado  ${posicoes[1]}`)
+    console.log(`3º - Colocado  ${posicoes[2]}`)
 
-    }
+}
 
-    console.log("Seguem as possibilidades de classificação:")
-
-    console.log("1 - Se o Daniel chegar em terceiro, o pódio poderá ficar assim: ", positions(player1, player2, player3))
-    console.log("Ou assim: ", positions(player2, player1, player3))
-
-    console.log("2 - Se o Daniel chegar em segundo, o pódio poderá ficar assim: ", positions(player3, player1, player2,))
-    console.log("Ou assim: ", positions(player3, player2, player1))
-
-    console.log("3 - Se o Daniel chegar em primeiro, o pódio repetirá os mesmos resultados da possibilidade 2.")
+console.log(positions("Daniel", "Ricardo", "Lucas"))
+console.log(positions("Daniel", "Lucas", "Ricardo"))
+console.log(positions("Ricardo", "Daniel", "Lucas"))
+console.log(positions("Ricardo", "Lucas", "Daniel"))
+console.log(positions("Lucas", "Daniel", "Ricardo"))
+console.log(positions("Lucas", "Ricardo", "Daniel"))
